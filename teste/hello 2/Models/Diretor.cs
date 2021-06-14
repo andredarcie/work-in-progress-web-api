2eb4f1c6bda6eff0fcbce02a1190d0278a64c2d7
andredarcie <andrendarcie@gmail.com>
2021-06-14 20:20:45 -0300

    Adicionar primeira versao

---
+++ teste/hello 2/Models/Diretor.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+public class Diretor {
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public ICollection<Filme> Filmes { get; set; }
+
+    public Diretor() {
+        Filmes = new List<Filme>();
+    }
+}
---
+++ teste/hello 2/Models/Filme.cs
@@ -0,0 +1,10 @@
+public class Filme {
+    public long Id { get; set; }
+    public string Titulo { get; set; }
+    public long DiretorId { get; set; }
+    public Diretor Diretor { get; set; }
+
+    public Filme(string titulo) {
+        Titulo = titulo;
+    }
+}
