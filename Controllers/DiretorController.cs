161259a3b7fd981cc6d56c132c2ccad9fdb1e5c1
andredarcie <andrendarcie@gmail.com>
2021-06-14 21:17:54 -0300

    Novos endpoints

--- Controllers/DiretorController.cs
+++ Controllers/DiretorController.cs
@@ -12,16 +12,44 @@ public class DiretorController : ControllerBase {
         _context = context;
     }
 
+    // GET api/diretores
     [HttpGet]
     public async Task<List<Diretor>> Get() {
         return await _context.Diretores.ToListAsync();
     }
 
+    // GET api/diretores/1
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Diretor>> Get(long id) {
+        var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
+        return Ok(diretor);
+    }
+
+    // POST api/diretores
     [HttpPost]
     public async Task<ActionResult<Diretor>> Post([FromBody] Diretor diretor) {
         _context.Diretores.Add(diretor);
         await _context.SaveChangesAsync();
 
+        return Ok(diretor);
+    }
+
+    // PUT api/diretores/{id}
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Diretor>> Put(long id, [FromBody] Diretor diretor) {
+        diretor.Id = id;
+        _context.Diretores.Update(diretor);
+        await _context.SaveChangesAsync();
+
+        return Ok(diretor);
+    }
+
+    // DELETE api/diretores/{id}
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(long id) {
+        var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
+        _context.Remove(diretor);
+        await _context.SaveChangesAsync();
         return Ok(diretor);
     }
 }
