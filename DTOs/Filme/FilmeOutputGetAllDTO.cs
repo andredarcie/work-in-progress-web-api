3b5e4420093a332d3d558aea79541c384c392846
andrendarcie <andrendarcie@gmail.com>
2021-06-28 21:31:12 -0300

    Adiciona DTOs para o filme

---
+++ DTOs/Filme/FilmeOutputGetAllDTO.cs
@@ -0,0 +1,9 @@
+public class FilmeOutPutGetAllDTO {
+    public long Id { get; set; }
+    public string Titulo { get; set; }
+
+    public FilmeOutPutGetAllDTO(long id, string titulo) {
+        Id = id;
+        Titulo = titulo;
+    }
+}
