9f9bd11efe90d3db917643ae6929044eecb1d31e
andrendarcie <andrendarcie@gmail.com>
2021-08-16 21:08:33 -0300

    Update UnitTest1.cs

--- MeuAppTests/UnitTest1.cs
+++ MeuAppTests/UnitTest1.cs
@@ -7,12 +7,32 @@ namespace MeuAppTests
     public class UnitTest1
     {
         [Fact]
-        public void NomeDoDiretorDeveApresentarErroSeForVazio()
+        public void NomeDoDiretorDeveApresentarErroSeForNull()
         {
             var validator = new DiretorInputPostDTOValidator();
             var dto = new DiretorInputPostDTO { Nome = null };
             var result = validator.TestValidate(dto);
             result.ShouldHaveValidationErrorFor(diretor => diretor.Nome);
+        }
+
+        [Fact]
+        public void NomeDoDiretorDeveApresentarErroSeForVazio()
+        {
+            var validator = new DiretorInputPostDTOValidator();
+            var dto = new DiretorInputPostDTO { Nome = "" };
+            var result = validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(diretor => diretor.Nome);
+        }
+
+        [Theory]
+        [InlineData("Teste")]
+        [InlineData("Teste Teste Teste Teste")]
+        public void NomeDoDiretorValidoNaoDeveConterErro(string data)
+        {
+            var validator = new DiretorInputPostDTOValidator();
+            var dto = new DiretorInputPostDTO { Nome = data };
+            var result = validator.TestValidate(dto);
+            result.ShouldNotHaveValidationErrorFor(diretor => diretor.Nome);
         }
     }
 }
